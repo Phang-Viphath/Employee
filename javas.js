b5fbfa3657b066921ba588a170942b52974486b4
Phang-Viphath <oooo099300823@gmail.com>
2025-03-11 14:04:18 +0700

    bonus

---
+++ javas.js
@@ -0,0 +1,25 @@
+function calculateBonus() {
+    var employeeID = document.getElementById('employeeID').value;
+    var employeeName = document.getElementById('employeeName').value;
+    var employeeSex = document.getElementById('employeeSex').value;
+    var employeeSalary = parseFloat(document.getElementById('employeeSalary').value);
+    var employeePosition = document.getElementById('employeePosition').value;
+
+    var bonusPercentage = 0;
+
+    if (employeePosition.toLowerCase() === "manager" || employeePosition.toLowerCase() === "MANAGER") {
+        bonusPercentage = 0.20;
+    } else if (employeePosition.toLowerCase() === "accounting" || employeePosition.toLowerCase() === "ACCOUNTING") {
+        bonusPercentage = 0.15;
+    } else if (employeePosition.toLowerCase() === "it" || employeePosition.toLowerCase() === "IT") {
+        bonusPercentage = 0.25;
+    } else {
+        bonusPercentage = 0.10;
+    }
+
+    var bonus = employeeSalary * bonusPercentage;
+    var totalSalary = employeeSalary + bonus;
+
+    document.getElementById('employeeBonus').textContent = `${(bonusPercentage * 100)}%`;
+    document.getElementById('TotalSalary').textContent = totalSalary;
+}
